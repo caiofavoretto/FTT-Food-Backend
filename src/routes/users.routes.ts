3e6da581eb63226d3d4ef698e3840593c894c310
Caio Favoretto <caiowfavoretto@gmail.com>
2020-05-23 19:54:23 -0300

    add: create users

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+
+import CreateUserService from '../services/CreateUserService';
+
+const userRouter = Router();
+
+userRouter.post('/', async (request, response) => {
+  const { name, last_name, email, registry, password } = request.body;
+
+  const createUserService = new CreateUserService();
+
+  const user = await createUserService.execute({
+    name,
+    last_name,
+    email,
+    registry,
+    password,
+  });
+
+  delete user.password_hash;
+
+  return response.json(user);
+});
+
+export default userRouter;
