1973ed086e37356a6b2c415864c278c5f3c717ce
Caio Favoretto <caiowfavoretto@gmail.com>
2020-06-16 11:01:57 -0300

    fix: server port

--- src/server.ts
+++ src/server.ts
@@ -33,6 +33,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   });
 });
 
-app.listen(3333, () => {
-  console.log('🚀 Server starded on port 3333');
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`🚀 Server starded on port ${process.env.PORT || 3333}`);
 });
